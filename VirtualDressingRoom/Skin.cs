5a7c56df3559c0311daafe3c0463df119592be13
Abdul Jabbar <abduljabbar5225@gmail.com>
2020-08-26 23:30:04 +0200

    Added final working project

---
+++ VirtualDressingRoom/Skin.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+
+namespace VirtualDressingRoom
+{
+    class Skin
+    {
+        private HaarCascade haarCascade, eyes;
+        BitmapImage bmp = new BitmapImage(new Uri(@"bin\Debug\frontal face.jpg"));
+        Bitmap bmp1;
+        int redval = 0, greenval = 0, blueval = 0;
+        private Bitmap BitmapImage2Bitmap(BitmapImage bmp)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BitmapEncoder encode = new BmpBitmapEncoder();
+                encode.Frames.Add(BitmapFrame.Create(bmp));
+                encode.Save(stream);
+                System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(stream);
+                return new Bitmap(bitmap);
+            }
+        }
+
+        public int SkinToneCalculate(Bitmap imageBitmap)
+        {
+            bmp1 = imageBitmap;
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(bmp1);
+            Image<Gray, Byte> grayimg = img.Convert<Gray, Byte>();
+            haarCascade = new HaarCascade(@"bin\Debug\haarcascade_frontalface_alt.xml");
+            eyes = new HaarCascade(@"bin\Debug\haarcascade_eye1.xml");
+            var detectedFaces = grayimg.DetectHaarCascade(haarCascade)[0];
+            int count = detectedFaces.Length;
+            if (count == 0)
+            {
+                return -1;
+            }
+            else
+            {
+                int x, y, locx, locy;
+                int facey = 0;
+                foreach (var face in detectedFaces)
+                {
+                    img.Draw(face.rect, new Bgr(255, 255, 255), 3);
+                    x = face.rect.Width;
+                    y = face.rect.Height;
+                    Debug.WriteLine(x + " new vals " + y);
+                    locx = face.rect.X;
+                    locy = face.rect.Y;
+                    facey = face.rect.Location.Y;
+                    var detectedeyes = grayimg.DetectHaarCascade(eyes)[0];
+                    int i1 = 0;
+                    int[] array = new int[100];
+                    foreach (var eye in detectedeyes)
+                    {
+                        img.Draw(eye.rect, new Bgr(0, 0, 0), 3);
+                        Debug.WriteLine("Eye nUmber: " + eye.rect.Location);
+                        array[i1] = eye.rect.Location.Y;
+                        i1++;
+                    }
+                    for (int i = 0; i < 100; i++) { if (array[i] == 0) { array[i] = 141525244; } }
+                    int minpix = array.Min();
+                    Debug.WriteLine(minpix);
+
+                    int mid = ((minpix - facey) / 2) + facey;
+                    double distance = x * 0.40;
+                    double distance1 = x - distance;
+                    int d = Convert.ToInt16(distance);
+                    int d1 = Convert.ToInt16(distance1);
+                    int redb = 0, greenb = 0, blueb = 0;
+                    int count1 = 0;
+                    for (int i = d; i < d1; i++)
+                    {
+                        redb = redb + img.Bitmap.GetPixel(i, mid).R;
+                        greenb = greenb + img.Bitmap.GetPixel(i, mid).G;
+                        blueb = blueb + img.Bitmap.GetPixel(i, mid).B;
+
+                        count1++;
+                    }
+
+                    int r1 = redb / count1;
+                    int g1 = greenb / count1;
+                    int b1 = blueb / count1;
+                    Debug.WriteLine(r1 + " " + g1 + " " + b1);
+                    //image1.Source = ToBitmapSource(img);
+                    Bitmap bmp2 = new Bitmap(50, 50);
+                    for (int i = 0; i < 50; i++)
+                    {
+                        for (int j = 0; j < 50; j++)
+                        {
+                            bmp2.SetPixel(i, j, System.Drawing.Color.FromArgb(r1, g1, b1));
+                        }
+                    }
+                    Image<Bgr, Byte> img1122 = new Image<Bgr, byte>(bmp2);
+                    //image.Source = ToBitmapSource(img1122);
+                    redval = r1;
+                    greenval = g1;
+                    blueval = b1;
+                }
+                return 1;
+            }
+
+
+        }
+
+        public int suggestskin()
+        {
+            int[,] vals = new int[6, 3] { { 255, 224, 196 }, { 255, 220, 178 }, { 238, 207, 179 }, { 227, 185, 143 }, { 222, 166, 118 }, { 106, 79, 60 } };
+            int[] final = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                int sum = 0;
+                sum = sum + Math.Abs(redval - vals[i, 0]);
+                sum = sum + Math.Abs(greenval - vals[i, 1]);
+                sum = sum + Math.Abs(blueval - vals[i, 2]);
+                final[i] = sum;
+                Debug.WriteLine(final[i]);
+            }
+            int minindex = Array.IndexOf(final, final.Min());
+            Debug.WriteLine(minindex);
+            Bitmap bmp2 = new Bitmap(50, 50);
+            return minindex;
+        }
+    }
+}
---
+++ VirtualDressingRoom/mainWindowMale.xaml.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using System.Diagnostics;
+
+namespace VirtualDressingRoom
+{
+    /// <summary>
+    /// Interaction logic for mainWindowMale.xaml
+    /// </summary>
+    public partial class mainWindowMale : Window
+    {
+        int pointmale = 1;
+        #region Members
+
+        KinectSensor _sensor;
+        MultiSourceFrameReader _reader;
+        IList<Body> bodies;
+        int gender;
+        #endregion
+
+        #region Constructor
+        string rightHandState1 = "";
+        string rightHandState2 = "";
+        string leftHandState1 = "";
+        string leftHandState2 = "";
+        #endregion
+        public mainWindowMale()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.Body);
+                _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (_reader != null)
+            {
+                _reader.Dispose();
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+        }
+        void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var reference = e.FrameReference.AcquireFrame();
+
+            // Body
+            using (var frame = reference.BodyFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+
+                    bodies = new Body[frame.BodyFrameSource.BodyCount];
+
+                    frame.GetAndRefreshBodyData(bodies);
+
+                    foreach (var body in bodies)
+                    {
+                        if (body != null)
+                        {
+                            if (body.IsTracked)
+                            {
+                                label.Content = "detected";
+                                bool check1 = false;
+                                bool check2 = false;
+                                // Find the joints
+                                Joint handRight = body.Joints[JointType.HandRight];
+                                Joint thumbRight = body.Joints[JointType.ThumbRight];
+                                Joint handLeft = body.Joints[JointType.HandLeft];
+                                Joint thumbLeft = body.Joints[JointType.ThumbLeft];
+                                Joint shoulder = body.Joints[JointType.ShoulderRight];
+                                Joint shoulderl = body.Joints[JointType.ShoulderLeft];
+                                Joint head = body.Joints[JointType.Head];
+
+                                //wave right
+                                // Hand above elbow
+                                if (body.Joints[JointType.HandRight].Position.Y > body.Joints[JointType.ElbowRight].Position.Y)
+                                {
+                                    // Hand right of elbow
+                                    if (body.Joints[JointType.HandRight].Position.X > body.Joints[JointType.ElbowRight].Position.X)
+                                    {
+                                        moverightmale(pointmale);
+                                        for (int i = 0; i < 2000; i++)
+                                        {
+                                            Debug.WriteLine("relax");
+                                        }
+                                    }
+
+                                    // Hand has not dropped but is not quite where we expect it to be, pausing till next frame
+                                    //return GesturePartResult.Undetermined;
+                                }
+                                //waveleft
+                                // hand above elbow
+                                if (body.Joints[JointType.HandLeft].Position.Y > body.Joints[JointType.ElbowLeft].Position.Y)
+                                {
+                                    // hand right of elbow
+                                    if (body.Joints[JointType.HandLeft].Position.X < body.Joints[JointType.ElbowLeft].Position.X)
+                                    {
+                                        // return GesturePartResult.Succeeded;
+                                        moveleftmale(pointmale);
+                                        for (int i = 0; i < 2000; i++)
+                                        {
+                                            Debug.WriteLine("relax");
+                                        }
+                                    }
+
+                                    // hand has not dropped but is not quite where we expect it to be, pausing till next frame
+                                    //return GesturePartResult.Undetermined;
+                                }
+
+
+
+
+                                ////Menu Right
+                                //if (thumbRight.Position.X < shoulderl.Position.X)
+                                //{
+                                //    moverightmale(pointmale);
+                                //    for (int i = 0; i < 2000; i++)
+                                //    {
+                                //        Debug.WriteLine("relax");
+                                //    }
+                                //}
+                                ////menuLeft
+                                //if (thumbLeft.Position.X > shoulder.Position.X)
+                                //{
+                                //    moveleftmale(pointmale);
+                                //    for (int i = 0; i < 2000; i++)
+                                //    {
+                                //        Debug.WriteLine("relax");
+                                //    }
+                                //}
+
+                                string rightHandState = "-";
+                                switch (body.HandRightState)
+                                {
+                                    case HandState.Open:
+                                        rightHandState = "Open";
+                                        break;
+                                    case HandState.Closed:
+                                        rightHandState = "Closed";
+                                        break;
+                                    case HandState.Lasso:
+                                        rightHandState = "Lasso";
+                                        break;
+                                    case HandState.Unknown:
+                                        rightHandState = "Unknown...";
+                                        break;
+                                    case HandState.NotTracked:
+                                        rightHandState = "Not tracked";
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                string leftHandState = "-";
+                                switch (body.HandLeftState)
+                                {
+                                    case HandState.Open:
+                                        leftHandState = "Open";
+                                        break;
+                                    case HandState.Closed:
+                                        leftHandState = "Closed";
+                                        break;
+                                    case HandState.Lasso:
+                                        leftHandState = "Lasso";
+                                        break;
+                                    case HandState.Unknown:
+                                        leftHandState = "Unknown...";
+                                        break;
+                                    case HandState.NotTracked:
+                                        leftHandState = "Not tracked";
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                //SELECT MENU
+                                if (rightHandState == "Closed" && handRight.Position.Y > head.Position.Y)
+                                {
+                                    for (int i = 0; i < 2000; i++)
+                                    {
+                                        Debug.WriteLine("relax");
+                                    }
+                                    if (_reader != null)
+                                    {
+                                        _reader.Dispose();
+                                    }
+
+                                    if (_sensor != null)
+                                    {
+                                        _sensor.Close();
+                                    }
+
+                                    btnselectionmale(pointmale);
+                                }
+                                //Select Back
+                                if (leftHandState == "Closed" && handLeft.Position.Y > head.Position.Y)
+                                {
+                                    for (int i = 0; i < 2000; i++)
+                                    {
+                                        Debug.WriteLine("relax");
+                                    }
+                                    if (_reader != null)
+                                    {
+                                        _reader.Dispose();
+                                    }
+
+                                    if (_sensor != null)
+                                    {
+                                        _sensor.Close();
+                                    }
+                                    this.Hide();
+                                    this.IsEnabled = false;
+                                    genderDetection genderObject = new genderDetection();
+                                    genderObject.Visibility = Visibility.Visible;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+        void initframe()
+        {
+            _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Infrared | FrameSourceTypes.Body);
+            _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+        }
+        void grabbed()
+        {
+            int c = 0;
+            Debug.WriteLine("Grabbed");
+            for (int i = 0; i < 3000; i++)
+            {
+                c++;
+                Debug.WriteLine(" ");
+            }
+        }
+        private void mwmbtnRightGesture_Click(object sender, RoutedEventArgs e)
+        {
+            moverightmale(pointmale);
+        }
+
+        private void moverightmale(int val)
+        {
+            if (val == 2)
+            {
+                mwmgrid1.Margin = new Thickness(386, 300, 0, 0);
+                mwmgrid3.Margin = new Thickness(708, 350, 0, 0);
+                pointmale = 1;
+            }
+            else if (val == 1)
+            {
+                mwmgrid1.Margin = new Thickness(386, 350, 0, 0);
+                mwmgrid3.Margin = new Thickness(708, 300, 0, 0);
+                pointmale = 2;
+            }
+            else
+            {
+            }
+        }
+
+        private void mwmbtnSelection_Click(object sender, RoutedEventArgs e)
+        {
+            if (_reader != null)
+            {
+                _reader.Dispose();
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+
+            btnselectionmale(pointmale);
+        }
+
+        private void btnselectionmale(int val)
+        {
+            if (val == 1)
+            {
+                this.Hide();
+                this.IsEnabled = false;
+                tryClothes tc = new tryClothes();
+                tc.Visibility = Visibility.Visible;
+            }
+            else if (val == 2)
+            {
+                this.Hide();
+                this.IsEnabled = false;
+                tryEye te = new tryEye();
+                te.Visibility = Visibility.Visible;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void mwmbtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            moveleftmale(pointmale);
+        }
+        private void moveleftmale(int val)
+        {
+            if (val == 2)
+            {
+                mwmgrid1.Margin = new Thickness(386, 300, 0, 0);
+                mwmgrid3.Margin = new Thickness(708, 350, 0, 0);
+                pointmale = 1;
+            }
+            else if (val == 1)
+            {
+                mwmgrid1.Margin = new Thickness(386, 350, 0, 0);
+                mwmgrid3.Margin = new Thickness(708, 300, 0, 0);
+                pointmale = 2;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void mwmbutton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_reader != null)
+            {
+                _reader.Dispose();
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+            this.Hide();
+            this.IsEnabled = false;
+
+            genderDetection genderObject = new genderDetection();
+            genderObject.Visibility = Visibility.Visible;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (_reader != null)
+            {
+                _reader.Dispose();
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+            this.Hide();
+            this.IsEnabled = false;
+            genderDetection genderObject = new genderDetection();
+            genderObject.Visibility = Visibility.Visible;
+        }
+    }
+}
